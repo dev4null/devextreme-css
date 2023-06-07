9c6452cbda5c49c22e85a0e31661e763d5f3848f
Vasiliy F <npopok2k@gmail.com>
2023-06-07 08:42:16 +0700

    css

--- src/views/grid.vue
+++ src/views/grid.vue
@@ -1,92 +1,113 @@
 <template>
   <div>
-    <h2 class="content-block">Tasks</h2>
 
-    <dx-data-grid
-      class="dx-card wide-card"
+ <h2 class="content-block">Tasks</h2>
+ <div style=" padding: 20px; width: 80%; height: 300px;">
+    <dx-data-grid    
       :data-source="dataSourceConfig"
-      :focused-row-index="0"
-      :show-borders="false"
-      :focused-row-enabled="true"
-      :column-auto-width="true"
-      :column-hiding-enabled="true"
-    >
+      :show-borders="true"
+      :height="500"
+      :show-row-lines="true">
       <dx-paging :page-size="10" />
-      <dx-pager :show-page-size-selector="true" :show-info="true" />
+      <dx-pager :show-page-size-selector="true" :show-info="true" :allowed-page-sizes="[10, 20, 50]" :show-navigation-buttons="true"/>
       <dx-filter-row :visible="true" />
+      <dx-selection  mode="multiple"/>
+      <dx-sorting mode="multiple"/>
 
-      <dx-column data-field="Task_ID" :width="90" :hiding-priority="2" />
+      <dx-column data-field="Task_ID" :width="80" />
+      <dx-column data-field="Task_Completion" caption="Completion" />
+      <dx-column data-field="Task_Subject" caption="Subject"/>
+      <dx-column data-field="Task_Description" caption="Description" :allow-sorting="false"/>
+      <dx-column data-field="Task_Status"  caption="Status" />
 
-      <dx-column
-        data-field="Task_Subject"
-        caption="Subject"
-        :width="190"
-        :hiding-priority="8"
-      />
-
-      <dx-column
-        data-field="Task_Status"
-        caption="Status"
-        :hiding-priority="6"
-      />
-
-      <dx-column
-        data-field="Task_Priority"
-        caption="Priority"
-        :hiding-priority="5"
-      >
-        <dx-lookup
-          display-expr="name"
-          value-expr="value"
-          :data-source="priorities"
-        />
+      <dx-column data-field="Task_Priority"  caption="Priority">
+        <dx-lookup display-expr="name"  value-expr="value" :data-source="priorities"/>
       </dx-column>
-
-      <dx-column
-        data-field="ResponsibleEmployee.Employee_Full_Name"
-        caption="Assigned To"
-        :allow-sorting="false"
-        :hiding-priority="7"
-      />
-
-      <dx-column
-        data-field="Task_Start_Date"
-        caption="Start Date"
-        data-type="date"
-        :hiding-priority="3"
-      />
-
-      <dx-column
-        data-field="Task_Due_Date"
-        caption="Due Date"
-        data-type="date"
-        :hiding-priority="4"
-      />
-
-      <dx-column
-        data-field="Task_Priority"
-        caption="Priority"
-        name="Priority"
-        :hiding-priority="1"
-      />
-
-      <dx-column
-        data-field="Task_Completion"
-        caption="Completion"
-        :hiding-priority="0"
-      />
+      <dx-column data-field="ResponsibleEmployee.Employee_Full_Name" caption="Assigned To" :allow-sorting="false" />
+      <dx-column data-field="Task_Start_Date"  caption="Start Date"  data-type="date"/>
     </dx-data-grid>
+    <br/>
+    <dx-data-grid    
+      :data-source="dataSourceConfig"
+      :show-borders="true"
+      :height="500"
+      :show-row-lines="true">
+      <dx-paging :page-size="10" />
+      <dx-pager :show-page-size-selector="true" :show-info="true" :allowed-page-sizes="[10, 20, 50]" :show-navigation-buttons="true"/>
+      <dx-filter-row :visible="true" />
+      <dx-selection  mode="multiple"/>
+      <dx-sorting mode="multiple"/>
+      <DxGroupPanel :visible="true"/>
+      <DxGrouping :auto-expand-all="expanded"/>
+      <DxToolbar>
+      <DxItem location="before" name="groupPanel"  />
+      <DxItem location="before" template="collapseTemplate" />
+      <DxItem location="after" template="btnNormal"/>
+      <DxItem location="after" template="btnIcon"/>
+      </DxToolbar>
+      <template #collapseTemplate>
+        <DxButton
+          :text="expanded ? 'Свернуть' : 'Развернуть'"
+          :height="56"
+          type="success"
+          styling-mode="secondary"
+          @click="collapseAllClick"
+        />
+      </template>
+      <template #btnIcon>
+          <DxButton
+              :height="56"
+              :width="56"
+              icon="floppy"
+              type="success"
+              styling-mode="contained"
+            />
+      </template>
+      <template #btnNormal>
+        <DxButton
+              :height="56"
+              text="TextText"
+              icon="link"
+              type="success"
+              styling-mode="contained"
+            />
+      </template>
+
+
+
+
+      <dx-column type="selection" [width]="50"></dx-column>
+      <dx-column data-field="Task_ID" :width="80" />
+      <dx-column data-field="Task_Completion" caption="Completion" />
+      <dx-column data-field="Task_Subject" caption="Subject"/>
+      <dx-column data-field="Task_Description" caption="Description" :allow-sorting="false"/>
+      <dx-column data-field="Task_Status"  caption="Status"  :group-index="0"/>
+
+      <dx-column data-field="Task_Priority"  caption="Priority">
+        <dx-lookup display-expr="name"  value-expr="value" :data-source="priorities"/>
+      </dx-column>
+      <dx-column data-field="ResponsibleEmployee.Employee_Full_Name" caption="Assigned To" :allow-sorting="false" />
+      <dx-column data-field="Task_Start_Date"  caption="Start Date"  data-type="date"/>
+    </dx-data-grid>
+  </div>
   </div>
 </template>
 
 <script>
 import "devextreme/data/odata/store";
+import { DxButton } from 'devextreme-vue/button';
 import DxDataGrid, {
   DxColumn,
   DxFilterRow,
   DxLookup,
   DxPager,
-  DxPaging
+  DxPaging,
+  DxSelection,
+  DxSorting,
+  DxGroupPanel,
+  DxGrouping,
+  DxToolbar,
+  DxItem,
 } from "devextreme-vue/data-grid";
 
 const priorities = [
@@ -99,8 +120,16 @@ const priorities = [
 export default {
   data() {
     return {
+      expanded: true,
       priorities
+      
     };
+  },
+  methods: {
+    collapseAllClick() {
+      this.expanded = !this.expanded;
+    },
+
   },
   created() {
     this.dataSourceConfig = {
@@ -113,6 +142,7 @@ export default {
       select: [
         "Task_ID",
         "Task_Subject",
+        "Task_Description",
         "Task_Start_Date",
         "Task_Due_Date",
         "Task_Status",
@@ -128,7 +158,27 @@ export default {
     DxFilterRow,
     DxLookup,
     DxPager,
-    DxPaging
+    DxPaging,
+    DxSelection,
+    DxSorting,
+    DxGroupPanel,
+    DxGrouping,
+    DxToolbar,
+    DxItem,
+    DxButton
   }
 };
 </script>
+<style>
+:root {
+  --lh: 1.4rem;
+}
+
+.truncate-overflow {
+  --max-lines: 3;
+  position: relative;
+  max-height: 60px;
+  overflow: hidden;
+  padding-right: 1rem;
+}
+</style>
